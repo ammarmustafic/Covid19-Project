868cd0b6f8c2d7869329b9e186d81999dfb3e12c
anesshare <anesshare@gmail.com>
2023-07-14 12:08:06 +0200

    Country-Stats

---
+++ src/components/pages/CountryStats/css.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+
+export const CardContainer = styled.div`
+width: 100%;
+height: 400px;
+display:flex;
+justify-content: center;
+align-items: center;
+flex-wrap: wrap;
+`
+export const Insert = styled.div`
+width: 100%;
+display: flex;
+justify-content: center;
+`
+export const MiniCard = styled.div`
+width:200px;
+height:200px;
+background-color:white;
+-webkit-box-shadow: -2px 6px 30px -9px rgba(0,0,0,0.75);
+-moz-box-shadow: -2px 6px 30px -9px rgba(0,0,0,0.75);
+box-shadow: -2px 6px 30px -9px rgba(0,0,0,0.75);
+margin:15px;
+display:flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+border-radius:20px;
+`
+
